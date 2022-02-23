317af8b0173bb97e131ab0fc7acf3409f810db71
Konstantin Shutkin <kostya80@gmail.com>
2022-02-23 23:58:51 +0400

    feat: implement subpackages to support some SPA libraries

---
+++ store/src/react.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import { createStoreFactory, Store } from './core';
+
+export const createStore = createStoreFactory(false);
+
+export const useStore = <T>(store: Store<T>) => {
+    const [, setState] = useState<object>();
+    
+    useEffect(() => {
+        return store(() => setState({}));
+    }, []);
+    
+    return store();
+};
