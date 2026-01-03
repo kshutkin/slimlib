96a7b510c9b4076ce580f0777ae2eeb7e89c8303
Konstantin Shutkin <kostya80@gmail.com>
2026-01-03 20:02:35 +0100

    remove extra library

--- injector/types/get-parameter-names.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'get-parameter-names' {
-    // biome-ignore lint/complexity/noBannedTypes: external module declaration
-    export default function getParameterNames(func: Function | string): string[];
-}
