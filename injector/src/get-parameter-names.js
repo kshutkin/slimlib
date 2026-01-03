6934cc7a3bf8e38669eff3f299e0ad2a68b94324
Konstantin Shutkin <kostya80@gmail.com>
2026-01-03 20:41:13 +0100

    refactoring

--- injector/src/get-parameter-names.js
+++ injector/src/get-parameter-names.js
@@ -5,67 +5,31 @@ const STRIP_PATTERN = /(static.*$)|(\/\/.*$)|(\/\*[\s\S]*?\*\/)|^\s*async(?:\s*|
 const nonVarChars = ['=', '(', ')', ','];
 
 /**
- * @typedef {Object} MatchResult
- * @property {string} subString - The substring between non-variable characters
- * @property {number} start - Start index of the substring
- * @property {number} end - End index of the substring
- */
-
-/**
  * Generator function that yields substrings between non-variable characters
  * @param {string} string - The string to parse
- * @returns {Generator<MatchResult, void, undefined>}
+ * @returns {Generator<string, void, undefined>}
  */
 function* matchNexter(string) {
-    /**
-     * Update the indexes of non-variable characters
-     * @param {number} stringIndex - Current string index
-     * @returns {number[]} Updated array of indexes
-     */
-    const updateIndex = stringIndex => {
-        return indexes.map((foundAt, i) => {
-            if (foundAt === stringIndex) {
-                return string.indexOf(/** @type {string} */ (nonVarChars[i]), foundAt + 1);
-            }
-            return foundAt;
-        });
-    };
-
-    /**
-     * Find the minimum index from the indexes array
-     * @returns {number} Minimum index or Infinity if none found
-     */
-    const minIndex = () => {
-        return Math.min.apply(
-            Math,
-            indexes.filter(i => i > -1)
-        );
-    };
-
     let indexes = nonVarChars.map(c => string.indexOf(c));
     let index = 0;
 
     while (index !== Infinity) {
-        const nextIndex = minIndex();
+        const nextIndex = Math.min.apply(
+            Math,
+            indexes.filter(i => i > -1)
+        );
         if (nextIndex !== Infinity) {
-            const subString = string.slice(index, nextIndex);
-            const ret = {
-                subString,
-                start: index,
-                end: nextIndex,
-            };
-            yield ret;
+            yield string.slice(index, nextIndex);
         } else if (string.length) {
-            const subString = string.slice(index);
-            const ret = {
-                subString,
-                start: index,
-                end: string.length,
-            };
-            yield ret;
+            yield string.slice(index);
         }
         index = nextIndex;
-        indexes = updateIndex(index);
+        indexes = indexes.map((foundAt, i) => {
+            if (foundAt === index) {
+                return string.indexOf(/** @type {string} */ (nonVarChars[i]), foundAt + 1);
+            }
+            return foundAt;
+        });
     }
 }
 
@@ -84,13 +48,13 @@ export default function parse(input) {
     let depthParenthesis = 0;
 
     const vars = [];
-    if (value?.subString?.length) {
-        vars.push(value.subString);
+    if (value?.length) {
+        vars.push(value);
     }
     for (value of gen) {
-        const firstChar = value.subString[0];
+        const firstChar = value[0];
         if (firstChar === '=') {
-            if (value.subString[1] === '>' && depthDefaultParams === 0) {
+            if (value[1] === '>' && depthDefaultParams === 0) {
                 break;
             } else {
                 depthDefaultParams++;
@@ -100,7 +64,7 @@ export default function parse(input) {
             depthParenthesis++;
         } else if (firstChar === '(' && firstVar) {
             vars.pop();
-            const newVar = value.subString.slice(1);
+            const newVar = value.slice(1);
             if (newVar.length) {
                 vars.push(newVar);
             }
@@ -110,7 +74,7 @@ export default function parse(input) {
         } else if (firstChar === ')' && depthParenthesis === 0) {
             break;
         } else if (firstChar === ',' || (firstChar === '(' && vars.length === 0)) {
-            const newVar = value.subString.slice(1);
+            const newVar = value.slice(1);
             if (depthParenthesis === 0) {
                 depthDefaultParams = 0;
                 vars.push(newVar);
