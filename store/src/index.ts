c7914db59ebe17bf3a377f15ba1b884b21aa7d6e
Konstantin Shutkin <kostya80@gmail.com>
2026-05-24 15:40:33 +0200

    export EffectOptions

--- store/src/index.ts
+++ store/src/index.ts
@@ -1,7 +1,7 @@
 export { computed } from './computed';
 export { flushEffects, untracked, unwrapValue } from './core';
 export { debugConfig, SUPPRESS_EFFECT_GC_WARNING, WARN_ON_UNTRACKED_EFFECT, WARN_ON_WRITE_IN_COMPUTED } from './debug';
-export { effect } from './effect';
+export { effect, EffectOptions } from './effect';
 export { activeScope, setActiveScope, setScheduler } from './globals';
 export { scope } from './scope';
 export { signal } from './signal';
