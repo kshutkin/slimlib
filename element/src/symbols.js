134be509e3a2d59a946811946a8e4089982b3ed1
Konstantin Shutkin <kostya80@gmail.com>
2026-05-31 00:03:22 +0200

    formatting

--- element/src/symbols.js
+++ element/src/symbols.js
@@ -6,7 +6,18 @@
  * inspect other message types.
  */
 
-export const [MOUNT, UNMOUNT, CONNECT, DISCONNECT, ADOPTED, MOVE, FORM_ASSOCIATED, FORM_DISABLED, FORM_RESET, FORM_STATE_RESTORE, RENDER_GEN] =
-    /** @type {[symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol]} */ (
-        Array.from({ length: 11 }, () => Symbol())
-    );
+export const [
+    MOUNT,
+    UNMOUNT,
+    CONNECT,
+    DISCONNECT,
+    ADOPTED,
+    MOVE,
+    FORM_ASSOCIATED,
+    FORM_DISABLED,
+    FORM_RESET,
+    FORM_STATE_RESTORE,
+    RENDER_GEN,
+] = /** @type {[symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol]} */ (
+    Array.from({ length: 11 }, Symbol)
+);
